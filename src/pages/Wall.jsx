1ff2e9dc909fca3647a21a26210e6189ee09375c
Karl George <karlgdev@gmail.com>
2023-01-18 13:43:02 +0100

    Fetching data from API

--- src/pages/Wall.jsx
+++ src/pages/Wall.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Wall = () => {
-  return <div>Wall</div>;
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <section>
+      <div className='container'>Wall</div>
+    </section>
+  );
 };
 
 export default Wall;
