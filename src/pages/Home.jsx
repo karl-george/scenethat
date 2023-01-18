c190436fab0028cb27703d083472724bea8a1412
Karl George <karlgdev@gmail.com>
2023-01-18 11:46:26 +0100

    Refactoring structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from '../components/Navbar/Navbar';
+import Wall from './Wall';
+
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Wall />
+      <Routes>
+        <Route path='/*' element={<Wall />} />
+      </Routes>
     </div>
   );
 };
