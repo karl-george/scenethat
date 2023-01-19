776cbb1e5072274cdd7ec4e8dd70051d4a6302b8
Karl George <karlgdev@gmail.com>
2023-01-19 12:50:13 +0100

    Working on styling movies

--- src/pages/Wall.jsx
+++ src/pages/Wall.jsx
@@ -4,12 +4,13 @@ import Movie from '../components/Movie/Movie';
 const Wall = () => {
   const [movieData, setMovieData] = useState();
   const [movieId, setMovieId] = useState();
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${
         import.meta.env.VITE_API_KEY
-      }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+      }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
     )
       .then((res) => res.json())
       .then((data) => setMovieData(data));
