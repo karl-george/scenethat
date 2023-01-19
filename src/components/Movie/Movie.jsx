7fa1787cdbd22e2de4926843bfaad316590fd1c7
Karl George <karlgdev@gmail.com>
2023-01-19 11:28:53 +0100

    Styling front page movies

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,7 +1,18 @@
 import './movie.css';
 
 const Movie = ({ movie }) => {
-  return <div>{movie.title}</div>;
+  const imageURL = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  const nameSliced = movie.title.slice(0, 35);
+
+  return (
+    <div className='movie__container'>
+      <img className='movie__img' src={imageURL} />
+      <div className='movie__details'>
+        <p className='movie__title'>{nameSliced}</p>
+        <p className='movie__rating'>{movie.vote_average}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
