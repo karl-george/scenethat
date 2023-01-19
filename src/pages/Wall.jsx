bb390ad60367010f687f745b3500e5024989b3d1
Karl George <karlgdev@gmail.com>
2023-01-19 13:57:09 +0100

    Setting up pagination

--- src/pages/Wall.jsx
+++ src/pages/Wall.jsx
@@ -1,20 +1,34 @@
 import { useEffect, useState } from 'react';
 import Movie from '../components/Movie/Movie';
+import ReactPaginate from 'react-paginate';
 
 const Wall = () => {
   const [movieData, setMovieData] = useState();
   const [movieId, setMovieId] = useState();
-  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const moviesPerPage = 20;
+  const offset = currentPage * moviesPerPage;
+  const currentMovies = movieData?.results?.slice(
+    offset,
+    offset + moviesPerPage
+  );
+
+  const pageCount = 500;
+
+  const paginate = ({ selected }) => {
+    setCurrentPage(selected + 1);
+  };
 
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${
         import.meta.env.VITE_API_KEY
-      }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}`
     )
       .then((res) => res.json())
       .then((data) => setMovieData(data));
-  }, []);
+  }, [currentPage]);
 
   const getMovieId = (id) => {
     setMovieId(id);
@@ -24,10 +38,27 @@ const Wall = () => {
     <Movie key={idx} movie={movie} getMovieId={getMovieId} />
   ));
 
+  console.log(movieData);
+
   return (
     <section>
       <div className='container'>
         <div className='movies__wrapper'>{movies}</div>
+        <ReactPaginate
+          onPageChange={paginate}
+          pageCount={pageCount}
+          previousLabel={'Prev'}
+          nextLabel={'Next'}
+          containerClassName={'pagination'}
+          pageLinkClassName={'page-number'}
+          previousLinkClassName={'page-number'}
+          nextLinkClassName={'page-number'}
+          activeLinkClassName={'active'}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          renderOnZeroPageCount={null}
+        />
+        {currentMovies}
       </div>
     </section>
   );
