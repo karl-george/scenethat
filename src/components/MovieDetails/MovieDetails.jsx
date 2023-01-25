24ace61ae72e5673dfd36defc7bdb5e0ba529d2d
Karl George <karlgdev@gmail.com>
2023-01-25 12:31:35 +0100

    Styling movie details hero

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -6,7 +6,17 @@ const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState();
   const { movieId } = useParams();
 
-  const imageURL = `https://image.tmdb.org/t/p/w500/${movieDetails?.backdrop_path}`;
+  // API only provides the end of the image path
+  const imageURL = `https://image.tmdb.org/t/p/original/${movieDetails?.backdrop_path}`;
+  const posterURL = `https://image.tmdb.org/t/p/w500/${movieDetails?.poster_path}`;
+
+  const production = movieDetails?.production_companies?.map(
+    (company) => company.name
+  );
+
+  const countries = movieDetails?.production_countries?.map(
+    (country) => country.name
+  );
 
   // Call API for specific movie details
   useEffect(() => {
@@ -19,13 +29,27 @@ const MovieDetails = () => {
       .then((data) => setMovieDetails(data));
   }, [movieId]);
 
-  console.log(movieDetails);
-
   return (
     <div>
       {movieDetails && (
-        <div>
-          <img src={imageURL} />
+        <div className='moviedetails'>
+          <img className='moviedetails__bg' src={imageURL} />
+          <div className='container moviedetails__content'>
+            <img className='moviedetails__poster' src={posterURL} />
+            <div className='moviedetails__content_column'>
+              <h2 className='moviedetails__title'>
+                {movieDetails.original_title}
+              </h2>
+              <p className='moviedetails__overview'>{movieDetails.overview}</p>
+              <p className='moviedetails__rating'>{movieDetails.popularity}</p>
+              <p className='moviedetails__release'>
+                {movieDetails.release_date}
+              </p>
+              <p className='moviedetails__runtime'>{movieDetails.runtime}</p>
+              <p className='moviedetails__runtime'>{production.join(', ')}</p>
+              <p className='moviedetails__runtime'>{countries.join(', ')}</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
