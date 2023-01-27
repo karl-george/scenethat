e8ca2e3ea76d38e617254288cc86d4b05e739024
Karl George <karlgdev@gmail.com>
2023-01-27 11:42:07 +0100

    Fix unneeded import

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Movie from '../Movie/Movie';
 import './moviedetails.css';
-import '../Movie/movie.css';
 
 const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState();
